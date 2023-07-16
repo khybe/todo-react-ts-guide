4d174ad5829dc674a55d372532a927df26be0274
Khaibar Saadat <khybersaadat93@gmail.com>
2023-07-16 17:15:23 +0200

    Exploring the Base Types

---
+++ basics.ts
@@ -0,0 +1,10 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects,
+// Function types, parameters
+
+// Primitives
+let age: number = 30;
+
+let userName: string = "Khaibar";
+
+let isStudent: boolean = true;
