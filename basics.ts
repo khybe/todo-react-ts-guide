f36dddd1e6f1e1b1df34d8b29f83c9bd67e39494
Khaibar Saadat <khybersaadat93@gmail.com>
2023-07-16 17:30:06 +0200

    Handling Array & Object Types in TypeScript

--- basics.ts
+++ basics.ts
@@ -8,3 +8,26 @@ let age: number = 30;
 let userName: string = "Khaibar";
 
 let isStudent: boolean = true;
+
+// More complex types
+let hobbies: string[];
+hobbies = ["Skating", "Kayaking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Khaibar",
+  age: 30,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+people = [
+  { name: "Max", age: 35 },
+  { name: "Khaibar", age: 30 },
+];
